51007c70d59f46ab504622489d99d33702b3c6a0
Jason Wuerch <wuerchjason@gmail.com>
2016-04-07 15:47:33 -0700

    pull individual records from database

--- app/routes/results.js
+++ app/routes/results.js
@@ -7,7 +7,11 @@ export default Ember.Route.extend({
     //API Urls
     var walmartURL = 'http://api.walmartlabs.com/v1/search?query=' + params.product + '&format=json&facet=on&apiKey=xekjrbwbrgm7822sk58zbxtg';
     var bestbuyURL = 'https://api.bestbuy.com/v1/products(search=' + params.product + ')?format=json&show=sku,name,salePrice&apiKey=8wd6evarbuf826pkknt76e39';
+    console.log(this.store.query('product', { filter: { name: 3.27 } })
+    .then(function(element) {
 
+      console.log(element.content[0]._data.name);
+    }));
     var store = this.store;
     //Saving to Database on each API call for BestBuy products
     Ember.$.getJSON(bestbuyURL).then(function(responseJSON) {
@@ -35,7 +39,6 @@ export default Ember.Route.extend({
 
     //Models that we can use on results.hbs
     return Ember.RSVP.hash({
-
       bestBuy: Ember.$.getJSON(bestbuyURL).then(function(responseJSON) {
         return responseJSON.products;
       }),
@@ -47,12 +50,6 @@ export default Ember.Route.extend({
   actions: {
     saveProduct(product) {
       this.get('shoppingCart').add(product);
-      var params1 = {
-        name: product.name,
-        salePrice: product.salePrice
-      };
-      var newProduct = this.store.createRecord('product', params1);
-      newProduct.save();
     },
     removeFromCart(product) {
       this.get('shoppingCart').remove(product);
