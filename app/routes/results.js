5fcf6cc735deffce6ae6e508f52153ccb15ef68d
Jason Wuerch <wuerchjason@gmail.com>
2016-04-06 17:01:16 -0700

    add shopping cart icon to nav bar

--- app/routes/results.js
+++ app/routes/results.js
@@ -3,10 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   shoppingCart: Ember.inject.service(),
   model: function(params) {
-    var url = 'http://api.walmartlabs.com/v1/search?query=' + params.product + '&format=json&facet=on&apiKey=xekjrbwbrgm7822sk58zbxtg';
-    return Ember.$.getJSON(url).then(function(responseJSON) {
-      console.log(responseJSON);
-      return(responseJSON.items);
+    var walmartURL = 'http://api.walmartlabs.com/v1/search?query=' + params.product + '&format=json&facet=on&apiKey=xekjrbwbrgm7822sk58zbxtg';
+    var bestbuyURL = 'https://api.bestbuy.com/v1/products(search=' + params.product + ')?format=json&show=sku,name,salePrice&apiKey=8wd6evarbuf826pkknt76e39';
+
+    return Ember.RSVP.hash({
+      bestBuy: Ember.$.getJSON(bestbuyURL).then(function(responseJSON) {
+        return responseJSON.products;
+      }),
+      walMart: Ember.$.getJSON(walmartURL).then(function(responseJSON) {
+        return responseJSON.items;
+      })
     });
   },
   actions: {
