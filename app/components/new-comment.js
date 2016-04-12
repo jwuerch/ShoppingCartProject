6d4873871de3dd445f4414c2b09cf6e6e1d8ac38
alex <alex.m.larson@gmail.com>
2016-04-12 16:51:39 -0700

    no gravitar

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -2,16 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   firebase: Ember.inject.service(),
+  userImage: "hey",
 
   actions: {
     saveComment() {
       var firebase = this.get('firebase');
-      // console.log(firebase.repo.auth.authData_.password.email);
+      console.log(firebase);
+      console.log(this.get('session'))
       var author = firebase.repo.auth.authData_.password.email
       var splitAuthor = author.split('');
       var newName = '';
-      console.log(splitAuthor);
-      debugger;
+
       for(var i = 0; i<splitAuthor.length; i++){
         if(splitAuthor[i] === "@"){
           break;
--- app/routes/product.js
+++ app/routes/product.js
@@ -3,12 +3,29 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   firebase: Ember.inject.service(),
 
+  
+
   model(params) {
+    // debugger;
     return this.store.findRecord('discuss', params.discuss_id);
   },
+
+
+  // model(params) {
+  //   return Ember.RSVP.hash({
+  //
+  //   product: this.store.findRecord('discuss', params.discuss_id),
+  //   image:
+  //
+  // })
+  // },
+
+
+
   actions: {
     saveComment(params) {
-
+      // var firebase = this.get('firebase');
+      // console.log(firebase);
       var newComment = this.store.createRecord('comment', params);
       var discuss = params.discuss;
       discuss.get('comments').addObject(newComment);
