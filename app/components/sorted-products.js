2bc6f14e066ff985dacff42036116ec7a3f0ab21
austinminnon <whi7ee@gmail.com>
2016-04-13 16:33:43 -0700

    add to cart functionality

--- app/components/sorted-products.js
+++ app/components/sorted-products.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
+
   sortBy: ['salePrice:asc'],
   sortedProducts: Ember.computed.sort('model', 'sortBy'),
   actions: {
     newDiscuss(discussionParams) {
       this.sendAction('newDiscuss', discussionParams);
+    },
+    saveProduct(product) {
+      this.get('shoppingCart').add(product);
     }
   }
 });
